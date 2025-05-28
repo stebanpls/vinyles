5974a8d4d63be57eafe43cb34f6925a7823a7b5f
stebanpls <stebanpulido@gmail.com>
2025-05-28 12:08:37 -0500

    Elimino todo otra vez.

--- gestion/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Crud # Importamos el modelo Crud desde el archivo models.py
-
-# Register your models here.
-admin.site.register(Crud) # Registra el modelo "Crud" en el panel de administración de Django.
