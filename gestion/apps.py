e16b4fe4d1e2f15db4d9de4af17bbfc3fa73e237
stebanpls <stebanpulido@gmail.com>
2025-06-09 11:27:41 -0500

    Cambio a apps.py

--- gestion/apps.py
+++ gestion/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class GestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestion'
+
+    def ready(self):
+        # Importar las señales para que se registren correctamente.
+        from . import models # Esto importará gestion/models.py donde está tu señal.
