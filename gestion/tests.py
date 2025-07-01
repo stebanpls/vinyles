d9c89deb73ae62e2bb9500ea7d9b85f40ed89171
stebanpls <stebanpulido@gmail.com>
2025-06-30 23:11:29 -0500

    Otra correccion.

--- gestion/tests.py
+++ gestion/tests.py
@@ -1,1 +1,7 @@
-# Create your tests here.
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_example(self):
+        """A very simple test to make sure the test runner works."""
+        self.assertEqual(1 + 1, 2)
